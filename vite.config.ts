ddcd4cf7bb00e2192b770a67f218e3b26191d236
anas97kamil <166991856+anas97kamil@users.noreply.github.com>
2025-12-03 05:31:11 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  // Fix: Property 'cwd' does not exist on type 'Process'
-  const env = loadEnv(mode, (process as any).cwd(), '');
+  const env = loadEnv(mode, '.', '');
+
   return {
     plugins: [react()],
     define: {
-      'process.env.API_KEY': JSON.stringify(env.API_KEY)
+      'import.meta.env.VITE_API_KEY': JSON.stringify(env.VITE_API_KEY)
     }
   };
 });
