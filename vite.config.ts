b71d0fadbb3ea167bf906af549b5c3aa6e8ff35c
anas97kamil <166991856+anas97kamil@users.noreply.github.com>
2025-12-03 05:55:00 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
+  const env = loadEnv(mode, process.cwd(), '');
 
   return {
     plugins: [react()],
-    define: {
-      'import.meta.env.VITE_API_KEY': JSON.stringify(env.VITE_API_KEY)
-    }
+
+    // أهم شي: ما منعرّف المتغيّر هون
+    // Vite لحالو بيعمل inject للـ import.meta.env
   };
 });
